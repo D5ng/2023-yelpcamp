f143bc7aa96cd8140c04c07d8444be8a57447bd4
d5ng <dong5ffice@gmail.com>
2023-01-26 23:06:30 +0900

    campground new & create

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Campground = require('./models/campground');
 
 
 // Mongoose 연결하기.
+mongoose.set('strictQuery', false);
 mongoose.connect('mongodb+srv://dongs:ehdgus1234@2023-yelp-camp.6h1bw49.mongodb.net/?retryWrites=true&w=majority')
 	.then(() => console.log('MongoDB Success'))
 	.catch(error => console.log(error));
@@ -14,6 +15,8 @@ const app = express();
 // Template을 ejs로 세팅.
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
@@ -26,18 +29,29 @@ app.get('/campgrounds', async (req, res) => {
 	res.render('campgrounds/index', { campgrounds });
 })
 
+app.get('/campgrounds/new', (req, res) => {
+	res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+	const campground = new Campground(req.body.campground)
+	await campground.save()
+	
+	res.redirect(`/campgrounds/${campground._id}`);
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
 	const id = req.params.id;
 	const campground = await Campground.findById(id);
 
-	res.render('campgrounds/detail', { campground });
+	res.render('campgrounds/show', { campground });
 })
 
-app.get('/makecampground', async (req, res) => {
-	const camp = new Campground({ title: "My Backyard" });
-	await camp.save();
-	res.send(camp);
+app.get('/campgrounds/:id/edit', async (req, res) => {
+	const campground = await Campground.findById(req.params.id)
+	res.render('campgrounds/edit', { campground })
 })
+
 
 
 app.listen(3000, () => console.log('listening on port 3000'));
