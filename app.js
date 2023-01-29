6eb06448dea462e7dbd944c78360aef978d3090a
d5ng <dong5ffice@gmail.com>
2023-01-29 19:27:05 +0900

    Header And Footer Style

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+
 
 // Mongoose 연결하기.
 mongoose.set('strictQuery', false);
@@ -18,6 +20,8 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.engine('ejs', ejsMate);
+
 
 app.get('/', (req, res) => {
 	res.render('./home.ejs');
